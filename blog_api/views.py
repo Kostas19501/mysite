eb793aca8b3c87a307001d9054951b5000319d16
Kostas19501 <konstantin1989volkov@mail.ru>
2025-08-11 15:31:46 +0500

    project mysite

---
+++ blog_api/views.py
@@ -0,0 +1,41 @@
+from rest_framework import generics, permissions
+from blog.models import Post
+from .serializers import PostSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+from .permissions import IsAuthorOrReadOnly
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [DjangoFilterBackend]  # new
+    filterset_fields = ['author']
+    search_fields = ['body', 'author__profile__bio']
+    ordering_fields = ['author_id', 'publish']
+    ordering = ['body']
+    pagination_class = StandardResultsSetPagination
+
+
+    #def get_queryset(self):
+     #   user = self.request.user
+      #  return Post.objects.filter(author=user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserPostList(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.kwargs['id']
+        return Post.objects.filter(author=user)
